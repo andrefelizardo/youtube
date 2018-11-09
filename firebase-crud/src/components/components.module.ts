c857f2025f3956b209a4f6eca75aa2cf16969fb4
André Felizardo <andreluis.felizardo@outlook.com>
2018-11-09 00:06:53 -0200

    Upload video 02 CRUD

--- firebase-crud/src/components/components.module.ts
+++ firebase-crud/src/components/components.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { ContatoFormComponent } from './contato-form/contato-form';
+import { IonicModule } from 'ionic-angular';
+import { MyApp } from '../app/app.component';
 @NgModule({
 	declarations: [ContatoFormComponent],
-	imports: [],
+	imports: [
+		IonicModule.forRoot(MyApp),
+	],
 	exports: [ContatoFormComponent]
 })
 export class ComponentsModule {}
--- firebase-crud/src/components/contato-form/contato-form.ts
+++ firebase-crud/src/components/contato-form/contato-form.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 /**
  * Generated class for the ContatoFormComponent component.
@@ -12,8 +13,24 @@ import { Component } from '@angular/core';
 })
 export class ContatoFormComponent {
 
-  constructor() {
+  contatoForm: FormGroup;
 
+  constructor(public formbuilder: FormBuilder) {
+    this.contatoForm = this.formbuilder.group({
+      nome: [null, [Validators.required, Validators.minLength(5)]],
+      empresa: [null, [Validators.minLength(3)]],
+      telefone: [null, [Validators.required, Validators.minLength(10), Validators.maxLength(11)]],
+      cep: [null, [Validators.required, Validators.minLength(8), Validators.maxLength(8)]],
+      rua: [null, [Validators.required]],
+      bairro: [null, [Validators.required]],
+      numero: [null],
+      cidade: [null, [Validators.required]],
+      uf: [null, [Validators.required]]
+    })
+  }
+
+  cadastraContato() {
+    console.log(this.contatoForm.value);
   }
 
 }
