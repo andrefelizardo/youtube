a71f94189d592ca5853fa1b1d73cf81c8392d7c9
André Felizardo <andreluis.felizardo@outlook.com>
2018-12-17 14:17:06 -0200

    upload video 08-9 CRUD

--- firebase-crud/src/pages/home/home.ts
+++ firebase-crud/src/pages/home/home.ts
@@ -7,6 +7,12 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  contatos = [
+    { nome: 'André Felizardo', empresa: 'Google', telefone: '2199999999', rua: 'Rua dos Alfeneiros', uf: 'RJ'},
+    { nome: 'Larissa Anitta', empresa: 'Microsoft', telefone: '21999984849', rua: 'Rua da Conciliação dos Evangélicos', uf: 'RJ'},
+    { nome: 'Harry Potter', empresa: 'Apple', telefone: '11999964778399', rua: 'Rua dos Dementadores de Grindewald', uf: 'SP'}
+  ]
+
   constructor(public navCtrl: NavController) {
 
   }
