d12890298de3913a4252fad4cbb027751aa26527
André Felizardo <andreluis.felizardo@outlook.com>
2018-12-22 07:57:35 -0200

    Upload video 11 CRUD

--- firebase-crud/src/pages/home/home.ts
+++ firebase-crud/src/pages/home/home.ts
@@ -7,20 +7,13 @@ import { Http } from '@angular/http';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  contatos = [
-    { nome: 'André Felizardo', empresa: 'Google', telefone: '2199999999', rua: 'Rua dos Alfeneiros', uf: 'RJ'},
-    { nome: 'Larissa Anitta', empresa: 'Microsoft', telefone: '21999984849', rua: 'Rua da Conciliação dos Evangélicos', uf: 'RJ'},
-    { nome: 'Harry Potter', empresa: 'Apple', telefone: '11999964778399', rua: 'Rua dos Dementadores de Grindewald', uf: 'SP'}
-  ];
-
   contatosDb;
 
   constructor(
     public navCtrl: NavController,
     public http: Http
     ) {
-
+      this.contatosDb = [];
   }
 
   ionViewDidLoad() {
@@ -31,8 +24,12 @@ export class HomePage {
     this.http.get('https://crud-ionic-e6e42.firebaseio.com/contatos.json')
     .map(res => res.json())
     .subscribe(data => {
-      console.log(data, 'dados do Firebase');
+      this.trataDados(data);
     })
+  }
+
+  trataDados(dados) {
+    this.contatosDb = Object.keys(dados).map(i => dados[i]);
   }
 
 }
